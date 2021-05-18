615865dc02c0a957e62fca687bc83cc6e3780368
Yajima Soichi <soichi513@icloud.com>
2021-05-18 21:35:23 +0900

    simple todo list

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="todo-item">
+    {{todo.label}}&nbsp;{{todo.done}}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+export interface Todo {
+  label: string
+  done: boolean
+}
+
+@Component
+export default class TodoItem extends Vue {
+  @Prop({required: true}) todo!: Todo
+  
+}
+</script>
+
+<style scoped>
+.todo-item {
+  border: 1px solid black;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div v-for="(item, index) in todos" :key="index">
+      <todo-item :todo="item" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import TodoItem, { Todo }  from '@/components/TodoItem.vue'
+
+@Component({
+  components: {
+    TodoItem
+  }
+})
+export default class TodoList extends Vue {
+  todos: Todo[] = [
+    {label: 'a', done: false},
+    {label: 'b', done: false},
+  ]
+}
+</script>
+
+<style></style>
